204bbc656b2bcd121d6a461cb814027430c6a030
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-09-19 11:54:37 +0000

    Fix travis trigger builds script

--- travis_scripts/trigger_builds.sh
+++ travis_scripts/trigger_builds.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo -n "Rebuild $REPO "
-url=$(echo $REPO | sed 's/\//%2F/g')
+URL=$(echo $REPO | sed 's/\//%2F/g')
 curl -X POST \
---write-out '%{http_code}' --silent --output \
+--write-out '%{http_code}' --silent --output /dev/null \
 -H "Content-Type: application/json" \
 -H "Travis-API-Version: 3" \
 -H "Accept: application/json" \
 -H "Authorization: token $TRAVIS_TOKEN" \
--d '{"request": {"branch": "master"}}' 'https://api.travis-ci.org/repo/$url/requests'
+-d '{"request": {"branch": "master"}}' https://api.travis-ci.org/repo/${URL}/requests
