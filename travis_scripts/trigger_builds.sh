91d2be07c9f341272c5d922517ca74595f4ee6d4
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-09-19 11:43:47 +0000

    Fix travis build again

--- travis_scripts/trigger_builds.sh
+++ travis_scripts/trigger_builds.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-echo -n "Rebuild distribrewed/master "
+echo -n "Rebuild $REPO "
+url=$(echo $REPO | sed 's/\//%2F/g')
 curl -X POST \
 --write-out '%{http_code}' --silent --output \
 -H "Content-Type: application/json" \
 -H "Travis-API-Version: 3" \
 -H "Accept: application/json" \
 -H "Authorization: token $TRAVIS_TOKEN" \
--d '{"request": {"branch": "master"}}' 'https://api.travis-ci.org/repo/distribrewed%2Fmaster/requests'
-echo ""
-
-echo -n "Rebuild distribrewed/workers "
-curl -X POST \
---write-out '%{http_code}' --silent --output \
--H "Content-Type: application/json" \
--H "Travis-API-Version: 3" \
--H "Accept: application/json" \
--H "Authorization: token $TRAVIS_TOKEN" \
--d '{"request": {"branch": "master"}}' 'https://api.travis-ci.org/repo/distribrewed%2Fworkers/requests'
+-d '{"request": {"branch": "master"}}' 'https://api.travis-ci.org/repo/$url/requests'
