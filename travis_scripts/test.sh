787990a8648cd7cccce90afcdb784924d08929d9
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-09-21 17:56:03 +0000

    Update coverage so it creates XML file

--- travis_scripts/test.sh
+++ travis_scripts/test.sh
@@ -7,8 +7,8 @@ DOCKER_DEV_FILE=$(pwd)/docker_dev/Dockerfile
 
 docker build -t ${TMP_IMAGE} -f ${DOCKER_DEV_FILE} .
 docker run -it -v $(pwd):/tmp/src -w /tmp/src/core_dev ${TMP_IMAGE} ash -c \
-	"set -e && coverage run --source=./distribrewed_core -m unittest discover -s ./tests/ && coverage report &&
-	mv .coverage /tmp/src/coverage.txt"
+	"set -e && coverage run --source=./distribrewed_core -m unittest discover -s ./tests/ && coverage report && coverage xml &&
+	mv coverage.xml /tmp/src/coverage.xml && rm .coverage"
 
 if [[ ${TRAVIS} == "true" ]]; then
    bash <(curl -s https://codecov.io/bash)
