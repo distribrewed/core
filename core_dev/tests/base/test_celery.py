1fae14aab0f997624c8be92e0a0211f1acb44068
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-09-24 23:20:53 +0000

    Improve tests for celery worker base class

--- core_dev/tests/base/test_celery.py
+++ core_dev/tests/base/test_celery.py
@@ -1,16 +1,39 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from distribrewed_core.base.celery import get_ip
+from distribrewed_core.base.celery import CeleryWorker
 
 
 class TestRun(TestCase):
+    @patch('distribrewed_core.base.celery.start_http_server')
+    @patch('distribrewed_core.base.celery.settings')
+    def test_on_ready(self, settings, mock_prom_server):
+        settings.AMQP_HOST = '127.0.0.1'
+        settings.AMQP_PORT = 0
+        settings.PROMETHEUS_SCRAPE_PORT = 9123
 
-    def setUp(self):
-        self.env = {
-            'AMQP_HOST': '127.0.0.1'
-        }
+        worker = CeleryWorker()
+        worker._on_ready()
 
-    def test_run_task(self):
-        with patch.dict('os.environ', self.env):
-            get_ip()
+        self.assertEqual(worker.ip, '127.0.0.1')
+        self.assertEqual(worker.prom_port, settings.PROMETHEUS_SCRAPE_PORT)
+        mock_prom_server.assert_called_with(settings.PROMETHEUS_SCRAPE_PORT)
+
+    @patch('distribrewed_core.base.celery.start_http_server')
+    @patch('distribrewed_core.base.celery.settings')
+    def test_on_ready_fail(self, settings, mock_prom_server):
+        settings.AMQP_HOST = '172.0.0.1'
+        settings.AMQP_PORT = 0
+        settings.PROMETHEUS_SCRAPE_PORT = 9123
+
+        def prom_server_fail(port):
+            raise Exception('Test prometheus server fail on port {}'.format(port))
+
+        mock_prom_server = prom_server_fail
+
+        worker = CeleryWorker()
+        worker._on_ready()
+
+    def test_on_shutdown(self):
+        worker = CeleryWorker()
+        worker._on_shutdown()
