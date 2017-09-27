22719971a6acf5b247af3853964a04e1de2de47e
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-09-27 18:57:11 +0000

    Make routing more customizable

--- core_dev/distribrewed_core/routes.py
+++ core_dev/distribrewed_core/routes.py
@@ -28,15 +28,25 @@ def all_workers_route():
 
 
 def route_task(name, args, kwargs, options, task=None, **kw):
-    route = {}
+    log.debug('Routing task {0}'.format(name))
+    log.debug('args: {}'.format(args))
+    log.debug('kwargs: {}'.format(kwargs))
+    log.debug('options: {}'.format(options))
+    log.debug('task: {}'.format(task))
+    log.debug('kw: {}'.format(kw))
+    route = {
+        'exchange': options.get('exchange'),
+        'exchange_type': options.get('exchange_type'),
+        'routing_key': options.get('routing_key')
+    }
     send_to_all_workers = options.get('all_workers')
     if send_to_all_workers:
         route = all_workers_route()
-    elif name.startswith('distribrewed_core.tasks.master'):
+    elif name.startswith('distribrewed_core.tasks.master') or options.get('routing_key', '') == 'master':
         route = master_route()
     elif name.startswith('distribrewed_core.tasks.worker'):
         wid = options.get('worker_id')
         assert wid is not None, 'When calling worker tasks you should specify worker_id'
         route = worker_route(wid)
-    log.debug('Routing: {0}'.format(route))
+    log.debug('Route {0}'.format(route))
     return route
