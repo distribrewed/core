2893c6865d43cd1810fefaba05043072e1064193
Guðmundur Björn Birkisson <gudmundurbb@gagnaveita.is>
2017-09-27 18:56:41 +0000

    Update build scripts

--- travis_scripts/build_arm.sh
+++ travis_scripts/build_arm.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -6,5 +6,8 @@ sed 's/\/base:x64/\/base:arm/g' Dockerfile > Dockerfile.arm
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -t distribrewed/core:arm -f Dockerfile.arm .
 docker run -t distribrewed/core:arm python --version
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push distribrewed/core:arm
+
+if [[ ${TRAVIS} == "true" ]]; then
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    docker push distribrewed/core:arm
+fi
--- travis_scripts/build_x64.sh
+++ travis_scripts/build_x64.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 docker build -t distribrewed/core:x64 -f Dockerfile .
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push distribrewed/core:x64
+
+if [[ ${TRAVIS} == "true" ]]; then
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    docker push distribrewed/core:x64
+fi
