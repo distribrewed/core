9c11451a65e1b3a573dbeb6ed459f68230c49d3f
Hilmar Finnsson <hif@simnet.is>
2017-11-22 22:18:58 +0000

    Update docker-entrypoint.sh
    Only set user as distribrewed if archtecture is 'arm'

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,9 +9,9 @@ fi
 
 # Drop root privileges if we are running celery
 # allow the container to be started with `--user`
-#if [ "$1" = 'celery' -a "$(id -u)" = '0' ]; then
-	#set -- su-exec distribrewed "$@"
-#fi
+if [ "$1" = 'celery' -a "$(id -u)" = '0' ] && [ ${ARCH} != 'arm']; then
+	set -- su-exec distribrewed "$@"
+fi
 
 # As argument is not related to celery,
 # then assume that user wants to run his own process,
