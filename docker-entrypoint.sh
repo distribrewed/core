9f05a6c7a62797431a1c13d37bee49b1571e1109
Hreidar Joelsson <hreidar@gmail.com>
2017-11-22 22:01:52 +0000

    Update docker-entrypoint.sh
    Run everything as root for testing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ fi
 # Drop root privileges if we are running celery
 # allow the container to be started with `--user`
 if [ "$1" = 'celery' -a "$(id -u)" = '0' ]; then
-	set -- su-exec distribrewed "$@"
+	#set -- su-exec distribrewed "$@"
 fi
 
 # As argument is not related to celery,
